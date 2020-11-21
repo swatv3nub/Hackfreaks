3e0a27473afa02bc49b3e06be712f36a0ad3bdbf
HellxGodLike <72937831+HellxGodLike@users.noreply.github.com>
2020-11-21 20:47:11 +0530

    Update userinfo.py

--- SaitamaRobot/modules/userinfo.py
+++ SaitamaRobot/modules/userinfo.py
@@ -453,7 +453,7 @@ def set_about_bio(update: Update, context: CallbackContext):
 
         if user_id == bot.id and sender_id not in DEV_USERS:
             message.reply_text(
-                "Erm... yeah, I only trust Heroes Association to set my bio.")
+                "Erm... yeah, I only trust SOLO•GUILD to set my bio.")
             return
 
         text = message.text
