8558601c3a63f63fdbf0662c316292b2d0096a02
HellxGodLike <72937831+HellxGodLike@users.noreply.github.com>
2020-11-24 13:38:24 +0530

    Update fun.py

--- SaitamaRobot/modules/fun.py
+++ SaitamaRobot/modules/fun.py
@@ -14,6 +14,7 @@ from SaitamaRobot.modules.helper_funcs.extraction import extract_user
 
 GIF_ID = 'CgACAgUAAx0EVmwfqQACElhfo3yZv1njCC11INcQSAi4UlN8vwACqwADg_8wVeGSv41OYU6zHgQ'
 
+PHOTO = 'https://i.imgur.com/UjiCJhZ.jpg'
 
 @run_async
 def runs(update: Update, context: CallbackContext):
@@ -40,16 +41,23 @@ def igris(update: Update, context: CallbackContext):
             temp = random.choice(fun_strings.IGRIS_STICKERS)
             reply_to.reply_sticker(temp)
         except BadRequest:
-            igris_type = "Text"   
+            igris_type = "Text" 
+
+@run_async
+def arise(update: Update, context: CallbackContext):
+    message = update.effective_message
+    name = message.reply_to_message.from_user.first_name if message.reply_to_message else message.from_user.first_name
+    reply_photo = message.reply_to_message.reply_photo if message.reply_to_message else message.reply_photo
+    reply_photo(PHOTO, caption=f'*Command Me {name}*')
                                                 
 @run_async
 def arise(update: Update, context: CallbackContext):
     message = update.effective_message
     name = message.reply_to_message.from_user.first_name if message.reply_to_message else message.from_user.first_name
     reply_photo = message.reply_to_message.reply_photo if message.reply_to_message else message.reply_photo
-    reply_photo(random.choice(fun_strings.IGRIS_IMG, caption=f'*Command Me {name}*')
+    reply_photo(
+        random.choice(fun_strings.IGRIS_IMG, caption=f'*Command Me {name}*')
          
-
 @run_async
 def truth(update: Update, context: CallbackContext):
     update.effective_message.reply_text(random.choice(fun_strings.TRUTH_STRINGS))
