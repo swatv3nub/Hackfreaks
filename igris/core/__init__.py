b2cd4bd313c3bddce8e82335a25c5e89a8bacbc2
HellxGodLike <72937831+HellxGodLike@users.noreply.github.com>
2020-11-25 18:43:30 +0530

    Update __init__.py

--- igris/core/__init__.py
+++ igris/core/__init__.py
@@ -13,4 +13,4 @@ from pyrogram import filters  # noqa
 from .database import get_collection  # noqa
 from .ext import pool  # noqa
 from .types.bound import Message  # noqa
-from .client import Userge  # noqa
+from .client import Igris  # noqa
