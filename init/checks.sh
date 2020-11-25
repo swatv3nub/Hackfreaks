16a68bdce9f261ffecb3d93e5b7509dc9ef82c2c
HellxGodLike <72937831+HellxGodLike@users.noreply.github.com>
2020-11-25 18:10:59 +0530

    Add files via upload

---
+++ init/checks.sh
@@ -0,0 +1,196 @@
+#!/bin/bash
+#
+# Copyright (C) 2020 by UsergeTeam@Github, < https://github.com/UsergeTeam >.
+#
+# This file is part of < https://github.com/UsergeTeam/Userge > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/uaudith/Userge/blob/master/LICENSE >
+#
+# All rights reserved.
+
+_checkBashReq() {
+    log "Checking Bash Commands ..."
+    command -v jq &> /dev/null || quit "Required command : jq : could not be found !"
+}
+
+_checkPythonVersion() {
+    log "Checking Python Version ..."
+    getPythonVersion
+    ( test -z $pVer || test $(sed 's/\.//g' <<< $pVer) -lt 3${minPVer}0 ) \
+        && quit "You MUST have a python version of at least 3.$minPVer.0 !"
+    log "\tFound PYTHON - v$pVer ..."
+}
+
+_checkConfigFile() {
+    log "Checking Config File ..."
+    configPath="config.env"
+    if test -f $configPath; then
+        log "\tConfig file found : $configPath, Exporting ..."
+        set -a
+        . $configPath
+        set +a
+        test ${_____REMOVE_____THIS_____LINE_____:-fasle} = true \
+            && quit "Please remove the line mentioned in the first hashtag from the config.sh file"
+    fi
+}
+
+_checkRequiredVars() {
+    log "Checking Required ENV Vars ..."
+    for var in API_ID API_HASH LOG_CHANNEL_ID DATABASE_URL; do
+        test -z ${!var} && quit "Required $var var !"
+    done
+    [[ -z $HU_STRING_SESSION && -z $BOT_TOKEN ]] && quit "Required HU_STRING_SESSION or BOT_TOKEN var !"
+    [[ -n $BOT_TOKEN && -z $OWNER_ID ]] && quit "Required OWNER_ID var !"
+    test -z $BOT_TOKEN && log "\t[HINT] >>> BOT_TOKEN not found ! (Disabling Advanced Loggings)"
+}
+
+_checkDefaultVars() {
+    replyLastMessage "Checking Default ENV Vars ..."
+    declare -rA def_vals=(
+        [WORKERS]=0
+        [PREFERRED_LANGUAGE]="en"
+        [DOWN_PATH]="downloads"
+        [UPSTREAM_REMOTE]="upstream"
+        [UPSTREAM_REPO]="https://github.com/code-rgb/USERGE-X"
+        [LOAD_UNOFFICIAL_PLUGINS]=true
+        [G_DRIVE_IS_TD]=true
+        [CMD_TRIGGER]="."
+        [SUDO_TRIGGER]="!"
+        [FINISHED_PROGRESS_STR]="█"
+        [UNFINISHED_PROGRESS_STR]="░"
+    )
+    for key in ${!def_vals[@]}; do
+        set -a
+        test -z ${!key} && eval $key=${def_vals[$key]}
+        set +a
+    done
+    DOWN_PATH=${DOWN_PATH%/}/
+    if [[ -n $HEROKU_API_KEY && -n $HEROKU_APP_NAME ]]; then
+        local herokuErr=$(runPythonCode '
+import heroku3
+try:
+    if "'$HEROKU_APP_NAME'" not in heroku3.from_key("'$HEROKU_API_KEY'").apps():
+        raise Exception("Invalid HEROKU_APP_NAME \"'$HEROKU_APP_NAME'\"")
+except Exception as e:
+    print(e)')
+        [[ $herokuErr ]] && quit "heroku response > $herokuErr"
+        declare -g HEROKU_GIT_URL="https://api:$HEROKU_API_KEY@git.heroku.com/$HEROKU_APP_NAME.git"
+    fi
+    for var in G_DRIVE_IS_TD LOAD_UNOFFICIAL_PLUGINS; do
+        eval $var=$(tr "[:upper:]" "[:lower:]" <<< ${!var})
+    done
+    local uNameAndPass=$(grep -oP "(?<=\/\/)(.+)(?=\@cluster)" <<< $DATABASE_URL)
+    local parsedUNameAndPass=$(runPythonCode '
+from urllib.parse import quote_plus
+print(quote_plus("'$uNameAndPass'"))')
+    DATABASE_URL=$(sed 's/$uNameAndPass/$parsedUNameAndPass/' <<< $DATABASE_URL)
+}
+
+_checkDatabase() {
+    editLastMessage "Checking DATABASE_URL ..."
+    local mongoErr=$(runPythonCode '
+import pymongo
+try:
+    pymongo.MongoClient("'$DATABASE_URL'").list_database_names()
+except Exception as e:
+    print(e)')
+    [[ $mongoErr ]] && quit "pymongo response > $mongoErr" || log "\tpymongo response > {status : 200}"
+}
+
+_checkTriggers() {
+    editLastMessage "Checking TRIGGERS ..."
+    test $CMD_TRIGGER = $SUDO_TRIGGER \
+        && quit "Invalid SUDO_TRIGGER!, You can't use $CMD_TRIGGER as SUDO_TRIGGER"
+}
+
+_checkPaths() {
+    editLastMessage "Checking Paths ..."
+    for path in $DOWN_PATH logs bin; do
+        test ! -d $path && {
+            log "\tCreating Path : ${path%/} ..."
+            mkdir -p $path
+        }
+    done
+}
+
+_checkBins() {
+    editLastMessage "Checking BINS ..."
+    declare -rA bins=(
+        [bin/megadown]="https://raw.githubusercontent.com/yshalsager/megadown/master/megadown"
+        [bin/cmrudl]="https://raw.githubusercontent.com/yshalsager/cmrudl.py/master/cmrudl.py"
+    )
+    for bin in ${!bins[@]}; do
+        test ! -f $bin && {
+            log "\tDownloading $bin ..."
+            curl -so $bin ${bins[$bin]}
+        }
+    done
+}
+
+_checkGit() {
+    editLastMessage "Checking GIT ..."
+    if test ! -d .git; then
+        if test ! -z $HEROKU_GIT_URL; then
+            replyLastMessage "\tClonning Heroku Git ..."
+            gitClone $HEROKU_GIT_URL tmp_git || quit "Invalid HEROKU_API_KEY or HEROKU_APP_NAME var !"
+            mv tmp_git/.git .
+            rm -rf tmp_git
+            editLastMessage "\tChecking Heroku Remote ..."
+            remoteIsExist heroku || addHeroku
+        else
+            replyLastMessage "\tInitializing Empty Git ..."
+            gitInit
+        fi
+        deleteLastMessage
+    fi
+}
+
+_checkUpstreamRepo() {
+    editLastMessage "Checking UPSTREAM_REPO ..."
+    remoteIsExist $UPSTREAM_REMOTE || addUpstream
+    replyLastMessage "\tFetching Data From UPSTREAM_REPO ..."
+    fetchUpstream || updateUpstream && fetchUpstream || quit "Invalid UPSTREAM_REPO var !"
+    fetchBranches
+    deleteLastMessage
+}
+
+_checkUnoffPlugins() {
+    editLastMessage "Checking USERGE-X [Extra] Plugins ..."
+    if test $LOAD_UNOFFICIAL_PLUGINS = true; then
+        editLastMessage "\tLoading USERGE-X [Extra] Plugins ..."
+        replyLastMessage "\t\tClonning ..."
+        gitClone --depth=1 https://github.com/code-rgb/Userge-Plugins.git
+        editLastMessage "\t\tUpgrading PIP ..."
+        upgradePip
+        editLastMessage "\t\tInstalling Requirements ..."
+        installReq Userge-Plugins
+        editLastMessage "\t\tCleaning ..."
+        rm -rf userge/plugins/unofficial/
+        mv Userge-Plugins/plugins/ userge/plugins/unofficial/
+        cp -r Userge-Plugins/resources/* resources/
+        rm -rf Userge-Plugins/
+        deleteLastMessage
+        editLastMessage "\tUSERGE-X [Extra] Plugins Loaded Successfully !"
+    else
+        editLastMessage "\tUSERGE-X [Extra] Plugins Disabled !"
+    fi
+    deleteLastMessage
+}
+
+assertPrerequisites() {
+    _checkBashReq
+    _checkPythonVersion
+    _checkConfigFile
+    _checkRequiredVars
+}
+
+assertEnvironment() {
+    _checkDefaultVars
+    _checkDatabase
+    _checkTriggers
+    _checkPaths
+    _checkBins
+    _checkGit
+    _checkUpstreamRepo
+    _checkUnoffPlugins
+}
---
+++ init/utils.sh
@@ -0,0 +1,123 @@
+#!/bin/bash
+#
+# Copyright (C) 2020 by UsergeTeam@Github, < https://github.com/UsergeTeam >.
+#
+# This file is part of < https://github.com/UsergeTeam/Userge > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/uaudith/Userge/blob/master/LICENSE >
+#
+# All rights reserved.
+
+declare -r minPVer=8
+declare -r maxPVer=10
+
+getPythonVersion() {
+    local -i count=$minPVer
+    local tmp
+    while true; do
+        tmp=$(python3.$count -V 2> /dev/null)
+        [[ -n $tmp || $count -gt $maxPVer ]] && break
+        count+=1
+    done
+    declare -gr pVer=$(sed -E 's/Python (3\.[0-9]{1,2}\.[0-9]{1,2}).*/\1/g' <<< $tmp)
+}
+
+log() {
+    local text="$*"
+    test ${#text} -gt 0 && test ${text::1} != '~' \
+        && echo -e "[$(date +'%d-%b-%y %H:%M:%S') - INFO] - init - ${text#\~}"
+}
+
+quit() {
+    local err="\t:: ERROR :: $1\nExiting With SIGTERM (143) ..."
+    if (( getMessageCount )); then
+        replyLastMessage "$err"
+    else
+        log "$err"
+    fi
+    exit 143
+}
+
+runPythonCode() {
+    python${pVer%.*} -c "$1"
+}
+
+runPythonModule() {
+    python${pVer%.*} -m "$@"
+}
+
+gitInit() {
+    git init &> /dev/null
+}
+
+gitClone() {
+    git clone "$@" &> /dev/null
+}
+
+remoteIsExist() {
+    grep -q $1 < <(git remote)
+}
+
+addHeroku() {
+    git remote add heroku $HEROKU_GIT_URL
+}
+
+addUpstream() {
+    git remote add $UPSTREAM_REMOTE ${UPSTREAM_REPO%.git}.git
+}
+
+updateUpstream() {
+    git remote rm $UPSTREAM_REMOTE && addUpstream
+}
+
+fetchUpstream() {
+    git fetch $UPSTREAM_REMOTE &> /dev/null
+}
+
+fetchBranches() {
+    local r_bs l_bs
+    r_bs=$(grep -oP '(?<=refs/heads/)\w+' < <(git ls-remote --heads $UPSTREAM_REMOTE))
+    l_bs=$(grep -oP '\w+' < <(git branch))
+    for r_b in $r_bs; do
+        [[ $l_bs =~ $r_b ]] || git branch $r_b $UPSTREAM_REMOTE/$r_b &> /dev/null
+    done
+}
+
+updateBuffer() {
+    git config http.postBuffer 524288000
+}
+
+upgradePip() {
+    pip3 install -U pip &> /dev/null
+}
+
+installReq() {
+    pip3 install -r $1/requirements.txt &> /dev/null
+}
+
+printLine() {
+    echo '->- ->- ->- ->- ->- ->- ->- --- -<- -<- -<- -<- -<- -<- -<-'
+}
+
+printLogo() {
+    printLine
+    echo '
+                 XXXXXXX       XXXXXXX                
+                 X:::::X       X:::::X                
+                 X:::::X       X:::::X                
+                 X::::::X     X::::::X                
+                 XXX:::::X   X:::::XXX                
+                    X:::::X X:::::X                   
+                     X:::::X:::::X                    
+ ---------------      X:::::::::X      ---------------
+ -:::::::::::::-      X:::::::::X      -:::::::::::::-
+ ---------------     X:::::X:::::X     ---------------
+                    X:::::X X:::::X                   
+                 XXX:::::X   X:::::XXX                
+                 X::::::X     X::::::X                
+                 X:::::X       X:::::X                
+                 X:::::X       X:::::X                
+                 XXXXXXX       XXXXXXX                
+'
+    printLine
+}
