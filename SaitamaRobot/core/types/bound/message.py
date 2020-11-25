435666454dff3fc0a3792142ab07ffa75761fadf
HellxGodLike <72937831+HellxGodLike@users.noreply.github.com>
2020-11-25 17:44:26 +0530

    Update message.py

--- SaitamaRobot/core/types/bound/message.py
+++ SaitamaRobot/core/types/bound/message.py
@@ -19,7 +19,7 @@ from pyrogram.errors.exceptions import MessageAuthorRequired, MessageTooLong
 from pyrogram.errors.exceptions.bad_request_400 import MessageNotModified, MessageIdInvalid
 from pyrogram.errors.exceptions.forbidden_403 import MessageDeleteForbidden
 
-from userge import logging
+from SaitamaRobot import logging
 from ... import client as _client  # pylint: disable=unused-import
 
 _CANCEL_LIST: List[int] = []
