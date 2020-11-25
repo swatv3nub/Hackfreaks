12e1f110f97bcccf95465544d291ba3fbe54fe23
HellxGodLike <72937831+HellxGodLike@users.noreply.github.com>
2020-11-25 18:32:16 +0530

    Add files via upload

---
+++ igris/__init__.py
@@ -0,0 +1,16 @@
+# pylint: disable=missing-module-docstring
+#
+# Copyright (C) 2020 by UsergeTeam@Github, < https://github.com/UsergeTeam >.
+#
+# This file is part of < https://github.com/UsergeTeam/Userge > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/uaudith/Userge/blob/master/LICENSE >
+#
+# All rights reserved.
+
+from userge.logger import logging  # noqa
+from userge.config import Config, get_version  # noqa
+from userge.core import (  # noqa
+    Userge, filters, Message, get_collection, pool)
+
+userge = Userge()  # userge is the client name
