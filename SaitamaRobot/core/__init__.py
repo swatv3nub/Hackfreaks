9824203258ef5880b6df95b98dee36b358779a63
HellxGodLike <72937831+HellxGodLike@users.noreply.github.com>
2020-11-25 18:05:13 +0530

    Add files via upload

---
+++ SaitamaRobot/core/__init__.py
@@ -0,0 +1,16 @@
+# pylint: disable=missing-module-docstring
+#
+# Copyright (C) 2020 by UsergeTeam@Github, < https://github.com/UsergeTeam >.
+#
+# This file is part of < https://github.com/UsergeTeam/Userge > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/uaudith/Userge/blob/master/LICENSE >
+#
+# All rights reserved.
+
+from pyrogram import filters  # noqa
+
+from .database import get_collection  # noqa
+from .ext import pool  # noqa
+from .types.bound import Message  # noqa
+from .client import Userge  # noqa
