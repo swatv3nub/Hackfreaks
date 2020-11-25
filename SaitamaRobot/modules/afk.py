d2a23e451da19cedd882f845b6f8e43e406fc09a
HellxGodLike <72937831+HellxGodLike@users.noreply.github.com>
2020-11-25 15:23:24 +0530

    Update afk.py

--- SaitamaRobot/modules/afk.py
+++ SaitamaRobot/modules/afk.py
@@ -43,8 +43,8 @@ def afk(update: Update, context: CallbackContext):
            ]
            chosen_option = random.choice(options) 
            update.effective_message.reply_text(chosen_option.format(firstname,notice) 
-       except:
-           return
+    except BadRequest:
+        pass
 
 
 @run_async
