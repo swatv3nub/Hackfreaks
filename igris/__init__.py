af4f290173412192daaaf15efe4140d3b9da1230
HellxGodLike <72937831+HellxGodLike@users.noreply.github.com>
2020-11-27 21:45:35 +0530

    Delete __init__.py

--- igris/__init__.py
+++
@@ -1,6 +0,0 @@
-from igris.logger import logging  # noqa
-from igris.config import Config, get_version  # noqa
-from igris.core import (  # noqa
-    Igris, filters, Message, get_collection, pool)
-
-igris = Igris()  # userge is the client name
