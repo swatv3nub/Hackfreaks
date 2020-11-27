d61fac8551a211d5a1c8e0a0b834a8cf27e44e42
HellxGodLike <72937831+HellxGodLike@users.noreply.github.com>
2020-11-27 13:10:27 +0530

    Update disasters.py

--- SaitamaRobot/modules/disasters.py
+++ SaitamaRobot/modules/disasters.py
@@ -626,7 +626,7 @@ dispatcher.add_handler(SUPPORTLIST_HANDLER)
 dispatcher.add_handler(SUDOLIST_HANDLER)
 dispatcher.add_handler(GUILD_HANDLER)
 
-__mod_name__ = "Disasters"
+__mod_name__ = "HUNTERS"
 __handlers__ = [
     SUDO_HANDLER, SUPPORT_HANDLER, TIGER_HANDLER, WHITELIST_HANDLER,
     UNSUDO_HANDLER, UNSUPPORT_HANDLER, UNTIGER_HANDLER, UNWHITELIST_HANDLER,
