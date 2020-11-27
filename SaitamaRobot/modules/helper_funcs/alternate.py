63bb96ba32eeea7571a1ed7b790dba3de3f9c6a4
HellxGodLike <72937831+HellxGodLike@users.noreply.github.com>
2020-11-27 23:55:29 +0530

    Add files via upload

--- SaitamaRobot/modules/helper_funcs/alternate.py
+++ SaitamaRobot/modules/helper_funcs/alternate.py
@@ -1,18 +1,23 @@
-import sys
-import traceback
-
+from telegram.error import BadRequest
 from functools import wraps
-from typing import Optional
-
-from telegram import User, Chat, ChatMember, Update, Bot
-from telegram import error
-
-from SaitamaRobot import DEL_CMDS, DRAGONS, WOLVES
+from telegram import ChatAction
 
 
-def send_message(message, text,  *args,**kwargs):
-	try:
-		return message.reply_text(text, *args,**kwargs)
-	except error.BadRequest as err:
-		if str(err) == "Reply message not found":
-			return message.reply_text(text, quote=False, *args,**kwargs)
+def send_message(message, text, *args, **kwargs):
+    try:
+        return message.reply_text(text, *args, **kwargs)
+    except BadRequest as err:
+        if str(err) == "Reply message not found":
+            return message.reply_text(text, quote=False, *args, **kwargs)
+
+
+def typing_action(func):
+    """Sends typing action while processing func command."""
+
+    @wraps(func)
+    def command_func(update, context, *args, **kwargs):
+        context.bot.send_chat_action(
+            chat_id=update.effective_chat.id, action=ChatAction.TYPING)
+        return func(update, context, *args, **kwargs)
+
+    return command_func
