841b11544f1526bd2cf94e9dc3e74b03f5b4d34f
sᴡᴏɴɪᴛ <72299804+swatv3nub@users.noreply.github.com>
2020-12-07 11:15:51 +0530

    [welcome.py] Welcome ranks

--- SaitamaRobot/modules/welcome.py
+++ SaitamaRobot/modules/welcome.py
@@ -171,7 +171,7 @@ def new_member(update: Update, context: CallbackContext):
             # Give the owner a special welcome
             if new_mem.id == OWNER_ID:
                 update.effective_message.reply_text(
-                    "Oh,Hunter Skill Immeasurable?Who is that Person?",
+                    "Oh,Fighting Skill Immeasurable?Who is that Person?",
                     reply_to_message_id=reply)
                 welcome_log = (f"{html.escape(chat.title)}\n"
                                f"#USER_JOINED\n"
@@ -181,7 +181,7 @@ def new_member(update: Update, context: CallbackContext):
             # Welcome Devs
             elif new_mem.id in DEV_USERS:
                 update.effective_message.reply_text(
-                    "Whoa! A member of the 🍁SOLO•GUILD🍁 just joined!",
+                    "Whoa! A member of the Dev Community just joined!",
                     reply_to_message_id=reply,
                 )
                 continue
@@ -189,7 +189,7 @@ def new_member(update: Update, context: CallbackContext):
             # Welcome Sudos
             elif new_mem.id in DRAGONS:
                 update.effective_message.reply_text(
-                    "GOD! A S-RANK HUNTER just joined! Stay Alert!",
+                    "GOD! An Elite just joined! Stay Alert!",
                     reply_to_message_id=reply,
                 )
                 continue
@@ -197,7 +197,7 @@ def new_member(update: Update, context: CallbackContext):
             # Welcome Support
             elif new_mem.id in DEMONS:
                 update.effective_message.reply_text(
-                    "Huh a A-RANK?! Someone with a A-RANK HUNTER SKILL just joined!",
+                    "Huh a Knight?! Someone with a Knight's Fighting Skill just joined!",
                     reply_to_message_id=reply,
                 )
                 continue
@@ -205,14 +205,14 @@ def new_member(update: Update, context: CallbackContext):
             # Welcome Whitelisted
             elif new_mem.id in TIGERS:
                 update.effective_message.reply_text(
-                    "OH YEAH! A B-RANK Hunter just joined!",
+                    "OH YEAH! A Horsemen just joined!",
                     reply_to_message_id=reply)
                 continue
 
             # Welcome Tigers
             elif new_mem.id in WOLVES:
                 update.effective_message.reply_text(
-                    "JEZZ! A C-RANK Hunter just joined!",
+                    "JEZZ! A Goblin just joined!",
                     reply_to_message_id=reply)
                 continue
 
