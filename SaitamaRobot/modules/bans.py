1e1e09629cb02c2be08758f3be542fc2aad72333
sᴡᴏɴɪᴛ <72299804+swatv3nub@users.noreply.github.com>
2020-12-07 11:11:55 +0530

    [bans.py]Ranks

--- SaitamaRobot/modules/bans.py
+++ SaitamaRobot/modules/bans.py
@@ -52,27 +52,27 @@ def ban(update: Update, context: CallbackContext) -> str:
     if is_user_ban_protected(chat, user_id, member) and user not in DEV_USERS:
         if user_id == OWNER_ID:
             message.reply_text(
-                "Trying to put me against a God level Hunter huh?")
+                "Trying to put me against a God level Fighter huh?")
             return log_message
         elif user_id in DEV_USERS:
             message.reply_text("I can't act against our own.")
             return log_message
         elif user_id in DRAGONS:
             message.reply_text(
-                "Fighting this S-RANK HUNTER here will put civilian lives at risk.")
+                "Fighting this Elite here will put civilian lives at risk.")
             return log_message
         elif user_id in DEMONS:
             message.reply_text(
-                "Bring an order from SOLO•GUILD to fight a A-RANK HUNTER."
+                "Bring an order from the EMPEROR to fight a Knight."
             )
             return log_message
         elif user_id in TIGERS:
             message.reply_text(
-                "Bring an order from SOLO•GUILD to fight a B-RANK HUNTER."
+                "Bring an order from the EMPEROR to fight a Horsemen."
             )
             return log_message
         elif user_id in WOLVES:
-            message.reply_text("C-RANK HUNTER abilities make them ban immune!")
+            message.reply_text("GOBLIN's abilities make them ban immune!")
             return log_message
         else:
             message.reply_text("This user has immunity and cannot be banned.")
