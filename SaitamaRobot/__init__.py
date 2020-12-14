f029dfe6675db37a387f703a7e8f1752dc59fbd1
sᴡᴏɴɪᴛ <72299804+swatv3nub@users.noreply.github.com>
2020-12-14 21:44:46 +0530

    Added STRICT_GMUTE
    Strict Gmute for gmuting the user when he speaks

--- SaitamaRobot/__init__.py
+++ SaitamaRobot/__init__.py
@@ -77,6 +77,7 @@ if ENV:
     NO_LOAD = os.environ.get("NO_LOAD", "translation").split()
     DEL_CMDS = bool(os.environ.get('DEL_CMDS', False))
     STRICT_GBAN = bool(os.environ.get('STRICT_GBAN', False))
+    STRICT_GMUTE = bool(os.environ.get('STRICT_GMUTE', False))
     WORKERS = int(os.environ.get('WORKERS', 8))
     BAN_STICKER = os.environ.get('BAN_STICKER',
                                  'CAADAgADOwADPPEcAXkko5EB3YGYAg')
