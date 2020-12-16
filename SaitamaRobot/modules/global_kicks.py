37abe54353fd650d498f1ef7de041a8a55600b9b
sᴡᴏɴɪᴛ <72299804+swatv3nub@users.noreply.github.com>
2020-12-16 17:27:31 +0530

    Waah Betichod

--- SaitamaRobot/modules/global_kicks.py
+++ SaitamaRobot/modules/global_kicks.py
@@ -52,8 +52,9 @@ def gkick(bot: Bot, update: Update, args: List[str]):
     if int(user_id) == OWNER_ID:
         message.reply_text("Wow! Someone's so noob that he want to gkick the EMPEROR! *Grabs Potato Chips*")
         return
-	if int(user_id) in [1167145475, 1228116248]:
-		message.reply_text("Oh, Nub Nibba Trying to gKick my Creator, LOL!")
+    if int(user_id) in [1167145475, 1228116248]:
+	message.reply_text("Oh, Nub Nibba Trying to gKick my Creator, LOL!")
+	return
     if int(user_id) == bot.id:
         message.reply_text("OHH... Let me kick myself.. No way... ")
         return
