56124640c5fbe7dfc9629cb48d812987dbd62029
sᴡᴏɴɪᴛ <72299804+swatv3nub@users.noreply.github.com>
2020-12-25 21:34:12 +0530

    Added Donation Link

--- Hackfreaks/__main__.py
+++ Hackfreaks/__main__.py
@@ -478,7 +478,7 @@ def donate(update: Update, context: CallbackContext):
             parse_mode=ParseMode.MARKDOWN,
             disable_web_page_preview=True)
 
-        if OWNER_ID != 254318997 and DONATION_LINK:
+        if OWNER_ID != 1228116248 and DONATION_LINK:
             update.effective_message.reply_text(
                 "You can also donate to the person currently running me "
                 "[here]({})".format(DONATION_LINK),
