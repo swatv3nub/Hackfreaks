67f27209cb3a6058b470147e2222dafb7ae9c755
sᴡᴏɴɪᴛ <72299804+swatv3nub@users.noreply.github.com>
2020-12-30 20:43:14 +0530

    Updating Extraction for Dynasty

--- Hackfreaks/modules/helper_funcs/extraction.py
+++ Hackfreaks/modules/helper_funcs/extraction.py
@@ -80,8 +80,7 @@ def extract_user_and_text(message: Message,
         if excp.message in ("User_id_invalid", "Chat not found"):
             message.reply_text(
                 "I don't seem to have interacted with this user before - please forward a message from "
-                "them to give me control! (like a voodoo doll, I need a piece of them to be able "
-                "to execute certain commands...)")
+                "them to give me control!")
         else:
             LOGGER.exception("Exception %s on user %s", excp.message, user_id)
 
@@ -95,7 +94,7 @@ def extract_text(message) -> str:
                                                if message.sticker else None)
 
 
-def extract_unt_fedban(message: Message,
+def extract_unt_dynastyban(message: Message,
                        args: List[str]) -> (Optional[int], Optional[str]):
     prev_message = message.reply_to_message
     split_text = message.text.split(None, 1)
@@ -125,7 +124,7 @@ def extract_unt_fedban(message: Message,
         user_id = get_user_id(user)
         if not user_id and not isinstance(user_id, int):
             message.reply_text(
-                "I don't have that user in my db.  "
+                "I don't have that user in my Database.  "
                 "You'll be able to interact with them if you reply to that person's message instead, or forward one of that user's messages."
             )
             return None, None
@@ -156,7 +155,6 @@ def extract_unt_fedban(message: Message,
             message.reply_text(
                 "I don't seem to have interacted with this user before "
                 "please forward a message from them to give me control! "
-                "(like a voodoo doll, I need a piece of them to be able to execute certain commands...)"
             )
             return None, None
         elif excp.message != "Chat not found":
