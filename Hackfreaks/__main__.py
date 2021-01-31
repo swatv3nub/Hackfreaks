e6f5ec77d46db88e0fcbc5daf8468404c83363ce
sᴡᴏɴɪᴛ <72299804+swatv3nub@users.noreply.github.com>
2021-01-31 20:13:53 +0530

    update.effective_message.reply_photo

--- Hackfreaks/__main__.py
+++ Hackfreaks/__main__.py
@@ -190,7 +190,7 @@ def start(update: Update, context: CallbackContext):
 
         else:
             first_name = update.effective_user.first_name
-            update.effective_message.reply_animation(
+            update.effective_message.reply_photo(
                 HACKFREAKS_IMG,
                 caption=PM_START_TEXT.format(escape_markdown(first_name), escape_markdown(context.bot.first_name), OWNER_ID),
                 parse_mode=ParseMode.MARKDOWN,
